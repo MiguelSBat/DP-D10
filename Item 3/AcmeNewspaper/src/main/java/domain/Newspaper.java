8bea3ad7b9d77c62d70c4a599fb1d5b08bd9c5e8
MiguelSBat <36421049+MiguelSBat@users.noreply.github.com>
2018-04-08 21:13:08 +0200

    Añadidas las anotaciones temporales a Newspaper

--- Item 3/AcmeNewspaper/src/main/java/domain/Newspaper.java
+++ Item 3/AcmeNewspaper/src/main/java/domain/Newspaper.java
@@ -9,10 +9,13 @@ import javax.persistence.AccessType;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -36,6 +39,8 @@ public class Newspaper extends DomainEntity {
 	}
 
 	@NotNull
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
 	public Date getPublicationDate() {
 		return this.publicationDate;
 	}
